377a7a2338930f85383de16a4e2a6d6748b14d71
Eric <makingentropy@gmail.com>
2017-09-15 12:49:52 -0500

    added dynamic_createMedia.html sans submit. Added ctrl.pageShowing

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ const app = angular.module("AppJS",[]);
 app.controller("appjsController", ['$http', function($http){
   const controller = this;
   this.test="test good";
-
+  this.pageShowing='includes/dynamic_createMedia.html';
 
 
 }]); //end rtController
